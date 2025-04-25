a63c7dec7827d72b0cbf0c8d78f2eebc5aca36ff
juanert <61994784+juanert@users.noreply.github.com>
2025-04-24 22:02:43 -0400

    Clase 1 javascript

---
+++ 5-javascript/section1.js
@@ -0,0 +1,23 @@
+// Comentario de una línea
+/*
+  Comentario de múltiples
+  lineas
+  :)
+*/
+/**
+ * Comentario de documentación
+ */
+console.log("Hola Mundo desde section1.js");
+let nombre
+
+/*
+  Tipos de datos
+  - String: "Hola Mundo", 'Hola Mundo', `Hola Mundo`
+  - Number: 1, 1.5, -1, -1.5
+  - Boolean: true, false
+  - Undefined: undefined
+  - Null: null
+  - Array: [1, 2, 3], ["Hola", "Mundo"]
+  - Object: { nombre: "Juan", edad: 30 }, { nombre: "Juan", edad: 30, amigos: ["Pedro", "Maria"] }
+  - Regexp: /[a-z]/, /[0-9]/, /[A-Z]/
+*/
