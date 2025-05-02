18fc0d981022029179f41896dd0190544b0c1743
juanert <61994784+juanert@users.noreply.github.com>
2025-05-01 22:57:56 -0400

    Javascript 6

--- 5-javascript/section2.js
+++ 5-javascript/section2.js
@@ -88,3 +88,43 @@ saludarConRetorno("Pedro"); //Ejecuta la funcion saludarConRetorno con el parame
 
 console.log(saludarConRetorno("Pedro"));
 console.log(saludarSinRetorno("Pedro"));
+
+/*
+  Recursion
+  La recursion es una técnica de programación donde una función se llama a sí misma para resolver un problema. 
+  Es útil cuando deseas ejecutar una tarea repetitiva o dividir un problema en subproblemas más pequeños.
+  Sin embargo, es importante tener cuidado con la recursion, ya que puede llevar a un desbordamiento de pila si 
+  no se maneja correctamente.
+  Para evitar esto, asegúrate de tener una condición de salida que detenga la recursion en algún momento.
+*/
+
+//Ejemplo de recursion, contador de n hasta y de uno en uno de forma ascendente
+function contar(n = 0, y = 0) {
+  if (n > y) {
+    return; //Condicion de salida
+  }
+  console.log(n); //Imprime el valor de n
+  contar(n + 1, y); //Llamada recursiva con n + 1
+}
+
+contar(1, 10); //Ejecuta la funcion contar desde 1 hasta 10
+contar(10, 1); //
+contar(5, 5);
+
+//Ejercicio fácil
+/*
+  Crea una función que cuente desde N hasta 0 de forma descendente y de dos en dos
+*/
+
+//Ejercicio intermedio
+/*
+  Crea una función que calcule el factorial de un número N. El factorial de un número N es el producto de todos los números enteros desde 1 hasta N.
+  Por ejemplo, el factorial de 5 es 5 * 4 * 3 * 2 * 1 = 120.
+*/
+
+//Ejercicio avanzado
+/*
+  Crea una función que calcule la serie de Fibonacci hasta el número N. La serie de Fibonacci es una secuencia de números donde cada número es la suma de los dos anteriores. 
+  Por ejemplo, la serie de Fibonacci hasta 10 es: 0, 1, 1, 2, 3, 5, 8.
+  La serie comienza con 0 y 1, y luego cada número es la suma de los dos anteriores.
+*/
