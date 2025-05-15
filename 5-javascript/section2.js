64902ca8446ac82f730b32bed8835522693a29ad
juanert <61994784+juanert@users.noreply.github.com>
2025-05-14 21:59:15 -0400

    Javascript programacion orientada a eventos

--- 5-javascript/section2.js
+++ 5-javascript/section2.js
@@ -99,12 +99,12 @@ console.log(saludarSinRetorno("Pedro"));
 */
 
 //Ejemplo de recursion, contador de n hasta y de uno en uno de forma ascendente
-function contar(n = 0, y = 0) {
-  if (n > y) {
+function contar(inicio = 0, limite = 0) {
+  if (inicio > limite) {
     return; //Condicion de salida
   }
-  console.log(n); //Imprime el valor de n
-  contar(n + 1, y); //Llamada recursiva con n + 1
+  console.log(inicio); //Imprime el valor de n
+  contar(inicio + 1, limite); //Llamada recursiva con n + 1
 }
 
 contar(1, 10); //Ejecuta la funcion contar desde 1 hasta 10
@@ -116,11 +116,38 @@ contar(5, 5);
   Crea una función que cuente desde N hasta 0 de forma descendente y de dos en dos
 */
 
+function contarDescendente(numero) {
+  if (numero < 0) {
+    return; //Condicion de salida
+  }
+  console.log(numero); //Imprime el valor de n
+  contarDescendente(numero - 2); //Llamada recursiva con n - 2
+}
+
+contarDescendente(10); //Ejecuta la funcion contarDescendente desde 10 hasta 0 de dos en dos
+
 //Ejercicio intermedio
 /*
   Crea una función que calcule el factorial de un número N. El factorial de un número N es el producto de todos los números enteros desde 1 hasta N.
   Por ejemplo, el factorial de 5 es 5 * 4 * 3 * 2 * 1 = 120.
 */
+
+function factorial(numero, contador = 1, acumulado = 0) {
+  if (contador == numero) {
+    return acumulado;
+  }
+  if (contador == 1) {
+    acumulado = numero * (numero - contador);
+  } else {
+    acumulado = acumulado * (numero - contador);
+  }
+  console.log(acumulado, contador, numero);
+  return factorial(numero, contador + 1, acumulado);
+}
+
+console.log(factorial(15));
+
+factorial(5); //Ejecuta la funcion factorial desde 5 hasta 1
 
 //Ejercicio avanzado
 /*
@@ -495,7 +522,7 @@ let ejemplo = [
   ],
 ];
 
-ejemplo[0][0].productos[0].tipos[1].nombre
+ejemplo[0][0].productos[0].tipos[1].nombre;
 
 //Metodos de arrays
 const frutas = ["manzana", "banana", "naranja", "uva", "pera"];
@@ -530,28 +557,386 @@ let ejemplo2 = function () {
   console.log("Hola"); //Imprime "Hola" en la consola
 };
 
-ejemplo2()
+ejemplo2();
 
 //Funciones flecha
 const ejemplo3 = () => {
   console.log("Hola"); //Imprime "Hola" en la consola
 };
 
-ejemplo3()
+ejemplo3();
 
-let frutas_5_letras = frutas.filter(
-  (fruta) => fruta.length > 5
-); //Filtra el array por longitud de la fruta
+let frutas_5_letras = frutas.filter((fruta) => fruta.length > 5); //Filtra el array por longitud de la fruta
 
 console.log(frutas_5_letras); //Imprime el array filtrado
 
-frutas.map(
-  (fruta) => {
-    console.log(fruta); //Imprime cada fruta del array
+frutas.map((fruta) => {
+  console.log(fruta); //Imprime cada fruta del array
+});
+
+//Ciclos o blucles
+/*
+  Los ciclos son estructuras de control que permiten ejecutar un bloque de código varias veces. 
+  Existen diferentes tipos de ciclos en JavaScript, como for, while y do while.
+*/
+//Ciclo for
+let carros = ["Mazda", "Toyota", "Chevrolet", "Renault", "Kia"]; //Array de carros
+for (let i = 0; i < carros.length; i++) {
+  console.log(carros[i]); //Imprime los numeros del 0 al 9
+}
+
+//ciclo for of (ciclo para recorrer arrays)
+for (let carro of carros) {
+  console.log(carro); //Imprime los nombres de los carros
+}
+
+//ciclo for in (ciclo para recorrer objetos)
+let persona = {
+  nombre: "Juan",
+  apellido: "Rodriguez",
+  edad: 30,
+  ciudad: "Bogota",
+};
+
+for (let propiedad in persona) {
+  console.log(persona[propiedad]); //Imprime las propiedades del objeto
+}
+
+//Ciclo while
+let contador = 20; //Contador
+while (contador < 10) {
+  console.log(contador); //Imprime los numeros del 0 al 9
+  contador++; //Aumenta el contador
+}
+
+//Ciclo do while
+let contador2 = 0; //Contador
+do {
+  console.log(contador2); //Imprime los numeros del 0 al 9
+  contador2++; //Aumenta el contador
+} while (contador2 < 10); //Condicion del ciclo
+
+//Ciclos dentro de ciclos
+let lista_de_compras = [
+  {
+    nombre: "Leche",
+    precio: 2.5,
+    cantidad: 1,
+  },
+  {
+    nombre: "Pan",
+    precio: 1.5,
+    cantidad: 2,
+  },
+  {
+    nombre: "Huevos",
+    precio: 3.0,
+    cantidad: 12,
+  },
+];
+
+for (let compra of lista_de_compras) {
+  console.log(compra); //Imprime el objeto de la compra
+  for (let propiedad in compra) {
+    console.log(compra[propiedad]); //Imprime las propiedades del objeto
   }
-);
+}
+
+// Pequeño ejercicio de arrrays dentro de arrays
+/*
+  Recorre el siguiente array
+  [{
+    categoria: "juegos de mesa"
+    juegos: ["monopoly", "ajedrez", "damas"]
+  },
+  {
+    categoria: "juegos de cartas"
+    juegos: ["poker", "uno", "baraja"]
+  },
+  {
+    categoria: "juegos de video"
+    juegos: ["fortnite", "call of duty", "fifa"]
+  }]
+*/
+
+/*
+  La POO (Programación Orientada a Objetos) es un paradigma de programación que se basa en la creación de objetos 
+  que contienen datos y métodos.
+*/
+
+let carro = {
+  marca: "Mazda",
+  modelo: "CX-5",
+  año: 2020,
+  color: "Rojo",
+  encender: function () {
+    console.log("El carro está encendido");
+  },
+  apagar: (dia) => {
+    console.log(`El carro está apagado el dia ${dia}`);
+  },
+};
+
+carro.apagar("miercoles"); //Ejecuta el metodo encender del objeto carro
+
+/* Clases,
+  Las clases son moldes de objetos.
+*/
+
+class Vehiculo {
+  constructor(
+    marca = "Mazda",
+    modelo,
+    anio,
+    color,
+    transmision = "automatico"
+  ) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+    this.color = color;
+    if (transmision == "automatico" || transmision == "manual") {
+      this.transmision = transmision;
+    } else {
+      this.transmision = "automatico";
+    }
+  }
+
+  informacion() {
+    console.log(
+      `El carro es de marca: ${this.marca}, modelo: ${this.modelo}, año: ${this.anio}, color: ${this.color}`
+    );
+  }
+
+  encender() {
+    console.log(`El carro ${this.marca} está encendido`);
+  }
+
+  apagar() {
+    console.log(`El carro ${this.marca} está apagado`);
+  }
+}
+
+let carro1 = new Vehiculo("Mazda", "CX-5", 2020, "Rojo", "automatico"); //Crea un nuevo objeto de la clase Carro
+
+carro1.informacion(); //Ejecuta el metodo informacion del objeto carro1
+carro1.encender(); //Ejecuta el metodo encender del objeto carro1
+carro1.apagar(); //Ejecuta el metodo apagar del objeto carro1
+
+let carro2 = new Vehiculo("Toyota", "Corolla", 2021, "Azul", "manual");
+carro2.informacion(); //Ejecuta el metodo informacion del objeto carro2
+carro2.encender(); //Ejecuta el metodo encender del objeto carro2
+carro2.apagar(); //Ejecuta el metodo apagar del objeto carro2
+
+/*
+  Herencia
+  La herencia es un mecanismo que permite crear nuevas clases a partir de clases existentes. 
+  Esto permite reutilizar código y crear jerarquías de clases.
+*/
+class Moto extends Vehiculo {
+  constructor(
+    marca,
+    modelo,
+    anio,
+    color,
+    transmision = "automatico",
+    cilindrada = 0
+  ) {
+    super(marca, modelo, anio, color, transmision);
+    this.cilindrada = cilindrada;
+  }
+
+  motopirueta() {
+    console.log(`La moto ${this.marca} está haciendo una pirueta`);
+  }
+}
+
+let moto1 = new Moto("Bera", "SBR", 2020, "Negra", "automatico"); //Crea un nuevo objeto de la clase Carro
+moto1.informacion(); //Ejecuta el metodo informacion del objeto carro1
+moto1.encender(); //Ejecuta el metodo encender del objeto carro1
+moto1.motopirueta(); //Ejecuta el metodo encender del objeto carro1
+moto1.apagar(); //Ejecuta el metodo apagar del objeto carro1
+
+/*
+  Realiza un videojuego utilizando clases y herencia.
+  Este videojuego debe tener las siguientes clases:
+  - Personaje
+    - Propiedades: nombre, vida, ataque, defensa, velocidad
+    - Metodos: Atacar, Saludar
+  - Mago
+    - Propiedades: hechizos (array de hechizos, cada hechizo tiene un nombre y un daño)
+    ejemplo de hechizo: {nombre: "Fuego", daño: 50}
+    - Metodos: Lanzar hechizo (seleccionar un hechizo aleatoriamente)
+  - Guerrero
+    - Propiedades: armas (array de armas, cada arma tiene un nombre y un daño)
+    - Metodos: Atacar con arma (seleccionar un arma aleatoriamente)
+    ejemplo de arma: {nombre: "Espada", daño: 30}
+  - Arquero
+    - Propiedades: flechas (array de flechas)
+    - Metodos: Disparar flecha
+
+  Debes de crear 5 personajes, 2 magos, 2 guerreros y 1 arquero.
+  Cada personaje debe de tener una vida, un ataque diferente, una defensa, velocidad aleatoria.
+
+  Crea un loop que permita a cada personaje atacar a otro personaje.
+  Al momento de realizar un ataque, el personaje que fue atacado debe de defenderse.
+
+  Cada vez que un personaje ataque a otro, se debe de imprimir el nombre del personaje que ataca,
+
+  Para calcular el daño que un personaje recibe se debe utilizar la siguiente formula:
+  Daño = (%Ataque del atacante) - (%Defensa del atacado)
+
+  Nota: El daño no puede ser menor a 0, y el ataque y defensa es un numero aleatorio entre
+  0 y el valor de ataque o defensa del personaje.
+
+  Cada vez que un personaje ataque a otro, se debe de imprimir el daño que recibe el personaje atacado
+  y quien ataca y quien es el atacado.
+  
+  Cuando un personaje ataca a otro, cada personaje debe atacar una vez al menos (Rondas),
+  sin embargo, el orden de cada ronda se determina aleatoriamente imprimiendo un numero
+  entre 0 y su velocidad.
+
+  Cuando la vida de un personaje llega a 0, se debe de imprimir que el personaje ha muerto.
+  y no puede seguir atacando.
+
+  Al final solo debe de quedar un personaje en pie.
+
+  Y debes imprimir un mensaje diciendo quien ha ganado.
+
+  Cada personaje realizara una acción aleatoria, es decir. Aleatoriamente puede atacar (de forma normal),
+  atacar con un hechizo o atacar con un arma. (Según el tipo de personaje).
+
+  Además, a quien ataca también se debe de seleccionar aleatoriamente.
+
+  Puedes utilizar metodos como Math.random() para seleccionar aleatoriamente un numero.
+
+  Math.random devuelve un numero entre 0 y 1, si quieres un numero entre 0 y 10, debes de multiplicar
+  el resultado por 11.
+
+  Math.floor() redondea un numero decimal hacia abajo. Es importante porque Math.random() devuelve
+  numeros decimales.
+
+  Ejemplo:
+  Math.floor(Math.random() * 10) //Devuelve un numero entre 0 y 9
+
+  Opcional:
+  - Que cada personaje tenga una habilidad especial que se pueda activar una vez por juego.
+  - Que cada personaje tenga una probabilidad de esquivar un ataque.
+  - Que cada personaje pueda tener items, estos pueden aumentar sus estadisticas o incluso curar al personaje
 
 
+  Fecha de entrega: 19/05/2025
+*/
 
+/*
+  Realiza el juego de buscaminas utilizando clases y herencia.
+  Este videojuego debe tener las siguientes clases:
 
+*/
 
+class Jugar {
+  constructor(nombre) {
+    this.nombre = nombre;
+    this.tablero = [];
+    this.tablero_jugador = [];
+    this.vidas = 2;
+    this.numero_de_movimientos = 0;
+  }
+
+  crearTablero() {
+    // Crear un tablero de 3x3
+    for (let i = 0; i < 3; i++) {
+      // Crear una fila
+      this.tablero[i] = [];
+      for (let j = 0; j < 3; j++) {
+        // Crear una celda
+        // 0 = celda vacia, 1 = celda con mina
+        this.tablero[i][j] = Math.floor(Math.random() * 2); // 0 o 1
+      }
+    }
+  }
+
+  crearTableroJugador() {
+    // Crear un tablero de 3x3
+    for (let i = 0; i < 3; i++) {
+      // Crear una fila
+      this.tablero_jugador[i] = [];
+      for (let j = 0; j < 3; j++) {
+        // Crear una celda
+        this.tablero_jugador[i][j] = " "; // 0 o 1
+      }
+    }
+  }
+
+  elegirCelda() {
+    let fila = prompt("Elige una fila (0, 1 o 2): ");
+    let columna = prompt("Elige una columna (0, 1 o 2): ");
+    // Validar que la fila y columna sean números entre 0 y 2
+    if (isNaN(fila) || isNaN(columna)) {
+      console.log("Error: Debes ingresar un número.");
+      this.elegirCelda();
+    } else if (fila < 0 || fila > 2 || columna < 0 || columna > 2) {
+      console.log("Error: Debes ingresar un número entre 0 y 2.");
+      this.elegirCelda();
+    }
+    return [fila, columna];
+  }
+
+  resultado(fila, columna) {
+    if (this.tablero[fila][columna] == 1) {
+      console.log("Has perdido una vida");
+      this.vidas--;
+      this.numero_de_movimientos++;
+      this.tablero_jugador[fila][columna] = "X";
+      if(this.morir()) {
+        console.log("Has perdido el juego");
+        return true;
+      }
+    } else {
+      console.log("No has muerto, aún tienes " + this.vidas + " vidas");
+      this.numero_de_movimientos++;
+      this.tablero_jugador[fila][columna] = "O";
+    }
+  }
+
+  morir() {
+    if (this.vidas == 0) {
+      console.log("Has muerto");
+      return true;
+    }
+    return false;
+  }
+
+  contarMinas() {
+    let contador = 0;
+    for (let i = 0; i < this.tablero.length; i++) {
+      for (let j = 0; j < this.tablero[i].length; j++) {
+        if (this.tablero[i][j] == 1) {
+          contador++;
+        }
+      }
+    }
+    return contador;
+  }
+
+  ganar() {
+    let numero_de_minas = this.contarMinas();
+    if (this.numero_de_movimientos == 9 - numero_de_minas) {
+      console.log("Has ganado");
+      return true;
+    }
+  }
+}
+
+let jugador1 = new Jugar("Juan"); //Crea un nuevo objeto de la clase Jugar
+jugador1.crearTablero(); //Ejecuta el metodo crearTablero del objeto jugador1
+jugador1.crearTableroJugador(); //Ejecuta el metodo crearTableroJugador del objeto jugador1
+console.log("Bienvenido al juego de buscaminas");
+console.log("El tablero tiene " + jugador1.contarMinas() + " minas");
+while (jugador1.vidas > 0){
+  let [fila, columna] = jugador1.elegirCelda(); //Ejecuta el metodo elegirCelda del objeto jugador1
+  jugador1.resultado(fila, columna); //Ejecuta el metodo resultado del objeto jugador1
+  console.log("Tablero jugador");
+  console.table(jugador1.tablero_jugador); //Imprime el tablero del jugador
+  console.log("Has hecho " + jugador1.numero_de_movimientos + " movimientos");
+}
